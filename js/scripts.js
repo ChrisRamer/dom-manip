8d960e58f10cacba96cf5d4dc1e5b1c427ffb475
Chris <chrisramer.97@gmail.com>
2020-08-03 14:03:26 -0700

    More advanced dom manip

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,15 @@ $(document).ready(function () {
 
 	$("button#hello").click(function (e) { 
 		$("ul#user").prepend("<li>Hello!</li>");
+		$("ul#user").children("li").first().click(function (e) { 
+			alert("Hey there!");
+			$(this).remove();
+		});
 		$("ul#webpage").prepend("<li>Why hello there!</li>");
+		$("ui#webpage").children("li").first().click(function (e) { 
+			alert("Hey there!");
+			$(this).remove();
+		});
 	});
 
 	$("button#goodbye").click(function (e) {
